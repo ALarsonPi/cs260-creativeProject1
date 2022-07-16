610d432f9a512f3f9e5a9ea65d1f20ab5b199d2e
Alec Larson <ALarsonPi@gmail.com>
2022-07-16 13:13:59 -0600

    Delete Desktop/Goals App/App Code/goals_app directory

--- Desktop/Goals App/App Code/goals_app/android/app/src/main/kotlin/com/example/goals_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.goals_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Desktop/Goals App/App Code/goals_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'AppRouter.dart';
-
-void main() {
-  runApp(const AppRouter());
-}
